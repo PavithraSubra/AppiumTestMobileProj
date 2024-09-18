e3775349f4982a4a07f5c04fdd064563a6a9a769
pavithra subramanian <pavithra.subramanian@gmail.com>
2024-09-18 11:03:35 +0530

    initial commit

---
+++ src/test/java/org/pavi/coder/mytestapp/AppTest.java
@@ -0,0 +1,17 @@
+package org.pavi.coder.mytestapp;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testAppium() {
+    	System.out.println("Hello TestNG");
+    }
+}
